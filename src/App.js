b99ec8c6aaa7c5864beebfdaeecb831b55509389
Sunil Prajapat <sunilpie1997@gmail.com>
2021-03-20 17:26:48 +0530

    2. Render Props (not covered)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import ClickCounter from './components/ClickCounter';
 import HoverCounter from './components/HoverCounter';
+import RenderProps from './components/RenderProps';
 
 function App() {
   return (
+
+    /* higher order components
     <div className="App">
-      
+    
       <HoverCounter name="Sunil"/>
       <ClickCounter name="Raju"/>
     </div>
+    /*
+
+    /* render props basic example */
+    <RenderProps name={(isLoggedIn)=>isLoggedIn? 'Sunil Prajapat':'Guest'}/>
   );
 }
 
